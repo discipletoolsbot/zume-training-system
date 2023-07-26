481f0f691dc15b5b0ccb1fe891c5f9742ecb0773
Chris Chasm <chris@chasm.solutions>
2023-07-26 10:50:54 -0600

    Added required composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '59441cf5b697361e74776450cfcb42538a98cdf0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '59441cf5b697361e74776450cfcb42538a98cdf0',
+    ),
+    'dealerdirect/phpcodesniffer-composer-installer' => 
+    array (
+      'pretty_version' => 'v1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4be43904336affa5c2f70744a348312336afd0da',
+    ),
+    'kucrut/vite-for-wp' => 
+    array (
+      'pretty_version' => 'v0.5.2',
+      'version' => '0.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '850e04b16b7bebfe6b1a276845d20bd99874fd56',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.7.2',
+      'version' => '3.7.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ed8e00df0a83aa96acf703f8c2979ff33341f879',
+    ),
+    'wp-coding-standards/wpcs' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7da1894633f168fe244afc6de00d141f27517b62',
+    ),
+  ),
+);
