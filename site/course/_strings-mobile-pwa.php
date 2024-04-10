f7b4b06265761f5ed0e82a0e444421bf00485c49
Chris Chasm <chris@chasm.solutions>
2024-04-10 11:48:07 -0600

    Remove unneeded files Added translation strings

---
+++ site/course/_strings-mobile-pwa.php
@@ -0,0 +1,47 @@
+<?php
+
+class Zume_PWA_Strings {
+    public function strings() {
+        /**
+         * These strings are to support the mobile PWA through inclusion into the POEditor translation workflow. 07/2020
+         */
+        $array = [
+            __( 'Yes', 'zume' ),
+            __( 'No', 'zume' ),
+            __( 'Home', 'zume' ),
+            __( 'Account', 'zume' ),
+            __( 'About', 'zume' ),
+            __( 'Settings', 'zume' ),
+            __( 'Downloads', 'zume' ),
+            __( 'Language', 'zume' ),
+            __( 'Reset App', 'zume' ),
+            __( 'Resetting the app will delete all app data. Are you sure you want to reset?', 'zume' ),
+            __( 'Help', 'zume' ),
+            __( 'Workbook', 'zume' ),
+            __( 'Sign Out', 'zume' ),
+            __( 'Download All', 'zume' ),
+            __( 'Download', 'zume' ),
+            __( 'Downloading', 'zume' ),
+            __( 'Queued', 'zume' ),
+            __( 'Delete', 'zume' ),
+            __( 'Do you want to clear these download errors?', 'zume' ),
+            __( 'Tap to persist', 'zume' ),
+            __( 'Select Language', 'zume' ),
+            __( 'Start Session', 'zume' ),
+            __( 'No sessions available', 'zume' ),
+            __( 'Exit', 'zume' ),
+            __( 'Previous', 'zume' ),
+            __( 'Next', 'zume' ),
+            __( 'Finish', 'zume' ),
+            __( 'Share', 'zume' ),
+            __( 'Install', 'zume' ),
+            __( 'This app can be installed to your device. Click to install this app', 'zume' ),
+            __( 'This app can be installed to your device. Please use the Chrome browser to do so.', 'zume' ),
+            __( 'Learn more', 'zume' ),
+            __( "Tap on Safari's share icon and tap 'Add to Home Screen' to install this app.", 'zume' ),
+            __( 'Update', 'zume' ),
+            __( 'A new version of this app is available. Click to update this app.', 'zume' ),
+            __( 'Storage', 'zume' ),
+        ];
+    }
+}
