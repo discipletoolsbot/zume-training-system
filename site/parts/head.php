533b36bc5b4528817ce96f14a425249612c403aa
squigglybob <squigglybob@users.noreply.github.com>
2024-04-16 14:33:41 +0100

    feat: add script codes for 3 test analytics pages (#281)

--- site/parts/head.php
+++ site/parts/head.php
@@ -1,3 +1,23 @@
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,700&display=swap');
 </style>
+<?php //phpcs:ignore ?>
+<script async src="https://ackee.gospelambition.com/tracker.js" data-ackee-server="https://ackee.gospelambition.com" data-ackee-domain-id="7754f83c-72b1-4126-bb31-72359d006deb"></script>
+<?php //phpcs:ignore ?>
+<script defer src="https://umami.gospelambition.com/script.js" data-website-id="e503fecf-00f2-4556-a7e3-40dd2671bdac"></script>
+
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://matomo.gospelambition.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
