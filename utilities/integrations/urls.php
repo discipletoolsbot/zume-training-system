4214c23dedfc75f91050e74a65fb3a93026430b0
Chris Chasm <chris@chasm.solutions>
2024-05-16 08:56:53 -0600

    Reordered the URL page. Preparing for refactor of url class and utility

--- utilities/integrations/urls.php
+++ utilities/integrations/urls.php
@@ -3,6 +3,7 @@ if ( !defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// resource functions
 function zume_get_url_pieces( $url = null ) {
 
     if ( !$url ) {
@@ -29,23 +30,18 @@ function zume_get_url_pieces( $url = null ) {
         'url_parts' => ( $url_parts ) ? $url_parts : [],
     ];
 }
-
 function zume_mirror_url() {
     return 'https://storage.googleapis.com/zume-file-mirror/';
 }
+function zume_url( $slug ) {
+    $current_lang = zume_current_language();
 
-function zume_home_url( $current_language = null ) {
-    if ( is_null( $current_language ) ) {
-        $current_language = zume_current_language();
+    if ( $current_lang === 'en' ) {
+        return site_url( '/' ) . $slug;
     }
-    if ( 'en' === $current_language ) {
-        $home_url = site_url();
-    } else {
-        $home_url = site_url() . '/' . $current_language;
-    }
-    return $home_url;
+
+    return site_url( '/' ) . $current_lang . '/' . $slug;
 }
-
 /**
  * Get the login/register url for zume, with an optional redirect url
  *
@@ -64,6 +60,9 @@ function zume_login_url( $type = 'login', $redirect_url = false ) {
 
     return dt_login_url( $type );
 }
+
+
+
 
 function zume_make_a_plan_wizard_url() {
     $redirect_url = zume_wizard_url( 'start' );
@@ -152,17 +151,9 @@ function zume_wizard_url( $type = 'start', $params = [] ) {
     return dt_create_site_url( $lang_code . '/' . $url, $params );
 }
 
-function zume_dashboard_url( $current_language = null ) {
-    if ( is_null( $current_language ) ) {
-        $current_language = zume_current_language();
-    }
-    $url = zume_get_posts_translation_url( 'Dashboard', $current_language );
-    return $url;
-}
 function zume_dashboard_page_url( $page ) {
     return zume_url( 'dashboard/' . $page );
 }
-
 function zume_checkin_url() {
     return zume_url( 'checkin' );
 }
@@ -175,7 +166,57 @@ function zume_invite_friends_url() {
 function zume_invite_training_url() {
     return zume_url( 'app/plan_invite' );
 }
+function zume_get_a_coach_url() {
+    return zume_url( 'get-a-coach' );
+}
+function zume_how_to_follow_jesus() {
+    return zume_url( 'how-to-follow-jesus' );
+}
+function zume_resources_url() {
+    return zume_url( 'resources' );
+}
+function zume_share_url() {
+    return zume_url( 'share' );
+}
+function zume_donate_url() {
+    return zume_url( 'donate' );
+}
+function zume_privacy_url() {
+    return zume_url( 'privacy' );
+}
+function zume_10_session_url() {
+    return zume_url( 'presenter?session=index' );
+}
+function zume_20_session_url() {
+    return zume_url( 'presenter?type=20&session=index' );
+}
+function zume_intensive_session_url() {
+    return zume_url( 'presenter?type=intensive&session=index' );
+}
 
+
+
+
+
+// Maybe redundant/legacy??
+function zume_home_url( $current_language = null ) {
+    if ( is_null( $current_language ) ) {
+        $current_language = zume_current_language();
+    }
+    if ( 'en' === $current_language ) {
+        $home_url = site_url();
+    } else {
+        $home_url = site_url() . '/' . $current_language;
+    }
+    return $home_url;
+}
+function zume_dashboard_url( $current_language = null ) {
+    if ( is_null( $current_language ) ) {
+        $current_language = zume_current_language();
+    }
+    $url = zume_get_posts_translation_url( 'Dashboard', $current_language );
+    return $url;
+}
 function zume_training_url( $current_language = null ) {
     if ( is_null( $current_language ) ) {
         $current_language = zume_current_language();
@@ -183,95 +224,52 @@ function zume_training_url( $current_language = null ) {
     $url = zume_get_posts_translation_url( 'Training', $current_language );
     return $url;
 }
-
 function zume_course_url() {
     $current_lang = zume_current_language();
     return zume_get_posts_translation_url( 'Course', $current_lang );
 }
-
-function zume_three_month_plan_url() {
-    $current_lang = zume_current_language();
-    return zume_get_posts_translation_url( 'Three-Month Plan', $current_lang );
-}
-
-function zume_mobile_app_url() {
-    return zume_url( 'mobile-app' );
-}
-
-function zume_follow_jesus_url() {
-    $current_lang = zume_current_language();
-    return zume_get_posts_translation_url( 'Follow Jesus', $current_lang );
-}
-
-function zume_get_a_coach_url() {
-    return zume_url( 'get-a-coach' );
-}
-
-function zume_how_to_follow_jesus() {
-    return zume_url( 'how-to-follow-jesus' );
-}
-
-function zume_vision_url() {
-    $current_lang = zume_current_language();
-    return zume_get_posts_translation_url( 'Vision', $current_lang );
-}
-
-function zume_overview_url() {
-    $current_lang = zume_current_language();
-    return zume_get_posts_translation_url( 'Overview', $current_lang );
-}
-
-function zume_resources_url() {
-    return zume_url( 'resources' );
-}
-
-function zume_share_url() {
-    return zume_url( 'share' );
-}
-function zume_donate_url() {
-    return zume_url( 'donate' );
-}
-
-function zume_privacy_url() {
-    return zume_url( 'privacy' );
-}
-
-function zume_profile_url() {
-    $current_lang = zume_current_language();
-    $url = zume_get_posts_translation_url( 'Profile', $current_lang );
-    return $url;
-}
-
 function zume_about_url() {
     $current_lang = zume_current_language();
     $url = zume_get_posts_translation_url( 'About', $current_lang );
     return $url;
 }
 
+
+
+
+
+
+
+
+/**
+ * @remove Deprecated functions below?? maybe?
+ */
 function zume_faq_url() {
     $current_lang = zume_current_language();
     $url = zume_get_posts_translation_url( 'FAQ', $current_lang );
     return $url;
 }
-
-function zume_10_session_url() {
-    return zume_url( 'presenter?session=index' );
-}
-
-function zume_20_session_url() {
-    return zume_url( 'presenter?type=20&session=index' );
-}
-
-function zume_intensive_session_url() {
-    return zume_url( 'presenter?type=intensive&session=index' );
-}
-
-function zume_url( $slug ) {
+function zume_profile_url() {
     $current_lang = zume_current_language();
-
-    if ( $current_lang === 'en' ) {
-        return site_url( '/' ) . $slug;
-    }
-
-    return site_url( '/' ) . $current_lang . '/' . $slug;
+    $url = zume_get_posts_translation_url( 'Profile', $current_lang );
+    return $url;
+}
+function zume_three_month_plan_url() {
+    $current_lang = zume_current_language();
+    return zume_get_posts_translation_url( 'Three-Month Plan', $current_lang );
+}
+function zume_mobile_app_url() {
+    return zume_url( 'mobile-app' );
+}
+function zume_follow_jesus_url() {
+    $current_lang = zume_current_language();
+    return zume_get_posts_translation_url( 'Follow Jesus', $current_lang );
+}
+function zume_overview_url() {
+    $current_lang = zume_current_language();
+    return zume_get_posts_translation_url( 'Overview', $current_lang );
+}
+function zume_vision_url() {
+    $current_lang = zume_current_language();
+    return zume_get_posts_translation_url( 'Vision', $current_lang );
 }
