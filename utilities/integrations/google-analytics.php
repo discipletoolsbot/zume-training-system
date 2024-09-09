2bc3e429be8c0dfcedd416f08f371c9ec1096d99
Chris Chasm <chris@chasm.solutions>
2024-09-09 07:50:36 -0600

    global vi language

---
+++ utilities/integrations/google-analytics.php
@@ -0,0 +1,16 @@
+<?php
+
+add_action( 'dt_blank_footer', function() {
+    ?>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-591815806">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-591815806');
+    </script>
+    <?php
+});
