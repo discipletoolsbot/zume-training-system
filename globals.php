c5f84dc83d8263c66b7bc10b9d2a1324a62756ca
Chris Chasm <chris@chasm.solutions>
2024-09-30 07:13:43 -0600

    telegu added

--- globals.php
+++ globals.php
@@ -1816,7 +1816,7 @@ if ( ! function_exists( 'zume_languages' ) ) {
                 'enable_flags' => [
                     'version_4_available' => true,
                     'translator_enabled' => true,
-                    'version_5_ready' => false,
+                    'version_5_ready' => true,
                     'pieces_pages' => true,
                     'course_slides_download' => false,
                 ],
